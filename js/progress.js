f7a5fb79a6f08aaab4f5c72394a6c7c1631e497e
VANTERC <18936151656@163.COM>
2017-11-22 13:10:39 +0800

    拖动 进度条更新

--- js/progress.js
+++ js/progress.js
@@ -1,7 +1,118 @@
-var scroll = document.getElementById('scroll');
-var bar = document.getElementById('bar');
-var mask = document.getElementById('mask');
-var ptxt = document.getElementsByTagName('p')[0];
+(function($){
+    var functions = {
+        init:function(input, step, max, min, digit){
+            var width = input.width()+30;
+            var height = input.width()/4;
+            var _this = this;
+            width += 3;
+
+            input.attr("readonly", "readonly");
+            //设置不分样式
+            input.css("border", "none");
+            input.css("width", width-height*2-50);
+            input.css("height", height);
+            input.css("padding", "0px");
+            input.css("margin", "0px");
+            input.css("text-align", "center");
+            input.css("vertical-align", "middle");
+            input.css("line-height", height + "px");
+            input.css("float","left");
+            input.css("font-size","15px");
+
+
+            //添加左右加减号
+            input.wrap("<div style = 'overflow:hidden;width:" + (width+5) + "px;height:" + height + "px;border: 1px solid #CCC;'></div>");
+            input.before("<div id = '" + input.attr('id') + "l'  onselectstart = 'return false;' style = '-moz-user-select:none;cursor:pointer;text-align:center;width:" + height + "px;height:" + height + "px;line-height:" + height + "px;border-right: 1px solid #CCC;float:left'>-</div>");
+
+            input.after("<div style='text-align:center;width:" + (height+21.5) + "px;height:" + height + "px;line-height:" + height + "px;float: left;position: relative;'>&nbsp;Mbps</div><div id = '" + input.attr('id') + "r'  onselectstart = 'return false;' style = '-moz-user-select:none;cursor:pointer;text-align:center;width:" + height + "px;height:" + height + "px;line-height:" + height + "px;border-left: 1px solid #CCC;float:right'> + </div>");
+            //左右调用执行
+            $("#" + input.attr('id') + "l").click(function(){
+                _this.execute(input, step, max, min, digit, true);
+                //通过值来计算出边距和宽度
+                var inputval =$("#progressval").val();
+                mask.style.width =(inputval/200)*(scroll.offsetWidth-bar.offsetWidth)+"px";
+                bar.style.left = (inputval/200)*(scroll.offsetWidth-bar.offsetWidth)+"px";
+
+            });
+            $("#" + input.attr('id') + "r").click(function(){
+                _this.execute(input, step, max, min, digit, false);
+                //通过值来计算出边距和宽度
+                var inputval =$("#progressval").val();
+                mask.style.width =(inputval/200)*(scroll.offsetWidth-bar.offsetWidth)+"px";
+                bar.style.left = (inputval/200)*(scroll.offsetWidth-bar.offsetWidth)+"px";
+            });
+        },
+        execute:function(input, step, max, min, digit, _do){
+            var val = parseFloat(this.format(input.val(), digit));
+            var ori = val;
+            if(_do) val -= step;
+            if(!_do) val += step;
+            if(val<min){
+                val  =  min;
+            }else if(val>max){
+                val  =  max;
+            }
+            input.val(this.format(val, digit));
+        },
+        format:function(val, digit){
+            if(isNaN(val)){
+                val = 0;
+            }
+            return parseFloat(val).toFixed(digit);
+        }
+    };
+
+
+    $(function(){
+        //使用控件必须有以下属性或者引用alignment类
+        var inputs = $("input[user_data], input[data_digit], input[data_step], input[data_min], input[data_max], input.alignment");
+        inputs.each(function(){
+            //预设值数据选择
+            var data = {
+                default_data : 	{"step" : 1, "min" : 0, "max" : 200, "digit" : 0},
+                aaa : 			{"step" : 0.5, "min" : 5, "max" : 20, "digit" : 1},
+            }
+
+            var user_data = eval("data." + $(this).attr("user_data"));
+            if(user_data == null){
+                user_data = data.default_data;
+            }
+
+            var digit = $(this).attr("data_digit");
+            if(digit != null&&!isNaN(parseFloat(digit))){
+                digit  =  parseFloat(digit).toFixed(0);
+                user_data.digit = parseFloat(digit);
+            }
+
+            var step = $(this).attr("data_step");
+            if(step != null &&!isNaN(parseFloat(step))){
+                user_data.step = parseFloat(step);
+            }
+            var min = $(this).attr("data_min");
+            if(min != null &&!isNaN(parseFloat(min))){
+                user_data.min = parseFloat(min);
+            }
+
+            var max = $(this).attr("data_max");
+            if(max != null &&!isNaN(parseFloat(max))){
+                user_data.max = parseFloat(max);
+            }
+            //自动装载
+            functions.init($(this), user_data.step, user_data.max, user_data.min, user_data.digit);
+
+            var data_edit = $(this).attr("data_edit");
+            if(data_edit){
+                $(this).attr("readonly",null);
+            }
+        });
+    })
+})(jQuery);
+
+
+var scroll = document.getElementById('barscroll');
+var bar = document.getElementById('barbtn');
+var mask = document.getElementById('masktools');
+
 var barleft = 0;
 bar.onmousedown = function(event){
     var event = event || window.event;
@@ -17,8 +128,8 @@ bar.onmousedown = function(event){
             barleft = scroll.offsetWidth - bar.offsetWidth;
         mask.style.width = barleft +'px' ;
         that.style.left = barleft + "px";
-        ptxt.innerHTML = "<div id='cyprotest'>" + parseInt(barleft/(scroll.offsetWidth-bar.offsetWidth) * 200) + "&nbsp;Mbps"+"</div>";
-
+        // jquery设置值
+        $("#progressval").val(parseInt(barleft/(scroll.offsetWidth-bar.offsetWidth) * 200));
         //防止选择内容--当拖动鼠标过快时候，弹起鼠标，bar也会移动，修复bug
         window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
     }
@@ -27,3 +138,10 @@ bar.onmousedown = function(event){
 document.onmouseup = function(){
         document.onmousemove = null; //弹起鼠标不做任何操作
     }
+
+$("#progressval").blur(function () {
+    var inputval =$("#progressval").val();
+    mask.style.width =(inputval/200)*(scroll.offsetWidth-bar.offsetWidth)+"px";
+    bar.style.left = (inputval/200)*(scroll.offsetWidth-bar.offsetWidth)+"px";
+})
+
